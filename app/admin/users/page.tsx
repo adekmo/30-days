ec47593688ec187fc5c94d511d258176234228a4
adekmo <monangbahana@gmail.com>
2025-05-13 20:27:58 +0700

    Delete User

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -31,6 +31,21 @@ const AdminUsersPage = () => {
         }
     }, [session]);
 
+    const handleDelete = async (id: string) => {
+      const confirm = window.confirm("Yakin ingin menghapus user ini?");
+      if (!confirm) return;
+
+        const res = await fetch(`/api/admin/users/${id}`, {
+          method: "DELETE",
+        });
+
+        if (res.ok) {
+        setUsers((prev) => prev.filter((r) => r._id !== id));
+        } else {
+        alert("Gagal menghapus user.");
+        }
+    };
+
 
   return (
     <div className="p-4">
@@ -43,7 +58,8 @@ const AdminUsersPage = () => {
               <th className="px-4 py-2">Nama</th>
               <th className="px-4 py-2">Email</th>
               <th className="px-4 py-2">Role</th>
-              <th className="px-4 py-2">Terdaftar</th>
+              <th className="px-4 py-2">Created At</th>
+              <th className="px-4 py-2">Action</th>
             </tr>
           </thead>
           <tbody>
@@ -60,6 +76,14 @@ const AdminUsersPage = () => {
                 <td className="px-4 py-2">{user.email}</td>
                 <td className="px-4 py-2">{user.role}</td>
                 <td className="px-4 py-2">{user.createdAt ? new Date(user.createdAt).toLocaleDateString() : "-"}</td>
+                <td className="px-2 py-2">
+                  <button
+                    className="mt-2 bg-red-600 text-white px-3 py-1 rounded hover:bg-red-700"
+                    onClick={() => handleDelete(user._id)}
+                  >
+                    ❌ Hapus
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
